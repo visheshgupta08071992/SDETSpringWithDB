3aff45fa6a655fa8ecfc22543dbddee9cccbc14a
AishwaryaGupta97 <aishu18gupta@gmail.com>
2021-11-24 15:50:24 +0530

    Added componentscan comment

--- src/main/java/SDETSpringWithDB/SDETSpringWithDB/SdetSpringWithDbApplication.java
+++ src/main/java/SDETSpringWithDB/SDETSpringWithDB/SdetSpringWithDbApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"SDETSpringWithDB.SDETSpringWithDB.Repository.CountryRepository","SDETSpringWithDB.SDETSpringWithDB.Entity.Country","SDETSpringWithDB.SDETSpringWithDB.Service.CountryService","SDETSpringWithDB.SDETSpringWithDB.Controller.CountryController"})
+
 public class SdetSpringWithDbApplication {
 
 	public static void main(String[] args) {
