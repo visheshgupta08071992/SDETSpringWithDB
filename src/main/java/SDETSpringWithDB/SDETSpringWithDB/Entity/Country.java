00a402873e6b66a8d3885ac05801f3e3dd9dd8b0
AishwaryaGupta97 <aishu18gupta@gmail.com>
2021-11-24 13:37:08 +0530

    Added Repository class which maps to Entity Country Class

--- src/main/java/SDETSpringWithDB/SDETSpringWithDB/Entity/Country.java
+++ src/main/java/SDETSpringWithDB/SDETSpringWithDB/Entity/Country.java
@@ -4,12 +4,60 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
-@AllArgsConstructor
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Country")
 public class Country {
 
+    @Id
+    @Column(name="id")
     int id;
+
+    @Column(name="countryname")
     String countryname;
+
+    @Column(name="countryCapital")
     String countryCapital;
+
+    public Country(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getCountryCapital() {
+        return countryCapital;
+    }
+
+    public void setCountryCapital(String countryCapital) {
+        this.countryCapital = countryCapital;
+    }
+
+
+    public Country(int id, String countryname, String countryCapital) {
+        this.id = id;
+        this.countryname = countryname;
+        this.countryCapital = countryCapital;
+    }
+
+
 }
